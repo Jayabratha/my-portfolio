54e235a1f62b7ace9a3d075ea292ae986a7d7eb2
Jayabratha Saha <jsaha1@sapient.com>
2017-01-21 11:39:17 +0530

    Making header fixed on scroll

--- src/app/js-onscroll.directive.ts
+++ src/app/js-onscroll.directive.ts
@@ -1,21 +1,23 @@
-import { Directive, ElementRef, HostListener, HostBinding } from '@angular/core';
+import { Directive, ElementRef, HostListener, HostBinding, EventEmitter, Output, Input } from '@angular/core';
 
 @Directive({
   selector: '[appJsOnscroll]'
 })
 export class JsOnscrollDirective {
-  elemOffsetTop: Number;
+  @Input() padding: number = 0;
+  @Output() onStateChange = new EventEmitter<string>();
+
+  elemOffsetTop: number;
   constructor(private el: ElementRef) { 
     this.elemOffsetTop = this.el.nativeElement.offsetTop;
+    console.log(this.elemOffsetTop);
   }
 
-  @HostBinding('class.fix-header') fixIt
   @HostListener('window:scroll') onScroll() {
-   // console.log("offsetTop: " + this.elemOffsetTop + ", scrollTop: " + document.body.scrollTop);
-    if (document.body.scrollTop > this.elemOffsetTop ) {
-      this.fixIt = true;
+    if (document.body.scrollTop > this.elemOffsetTop - this.padding ) {
+      this.onStateChange.emit('fix');
     }  else {
-      this.fixIt = false;
+      this.onStateChange.emit('scroll');
     } 
   }
 }
--- src/app/me-card/me-card.component.ts
+++ src/app/me-card/me-card.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-me-card',
+  selector: 'me-card',
   templateUrl: './me-card.component.html',
   styleUrls: ['./me-card.component.css']
 })
-export class MeCardComponent implements OnInit {
+export class MeCardComponent {
 
-  constructor() { }
+  isHeaderFix : boolean;
 
-  ngOnInit() {
+  headerStateChange(state: string) {
+    if (state === 'fix') {
+      this.isHeaderFix = true;
+    } else if (state === 'scroll') {
+      this.isHeaderFix = false;
+    }
   }
+  // constructor() { }
+
+  // ngOnInit() {
+  // }
+
 
 }
