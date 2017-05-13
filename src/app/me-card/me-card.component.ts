22deccf97dde8f6eb47b4e8b6956066bb9a8e6a0
Jayabratha Saha <js.design@outlook.com>
2017-05-13 19:13:42 +0530

    Fixes

--- src/app/me-card/me-card.component.ts
+++ src/app/me-card/me-card.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy } from '@angular/core';
 import { AppStateService } from '../app-state.service';
-import { Subscription }   from 'rxjs/Subscription';
-import { Router, NavigationStart }    from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'me-card',
@@ -12,7 +12,7 @@ export class MeCardComponent implements OnDestroy {
 
   isInitial: boolean = true;
   showMenu: boolean = false;
-  isHeaderFix : boolean;
+  isHeaderFix: boolean;
   activeNav: string = "";
   subscription: Subscription;
   activateScroll: boolean = true;
@@ -25,25 +25,25 @@ export class MeCardComponent implements OnDestroy {
     title: "My pic 1",
     alt: "My pic 1",
     description: ""
-  },{
+  }, {
     id: "slide2",
     url: "assets/images/my-pic1.jpg",
     title: "My pic 2",
     alt: "My pic 2",
     description: ""
-  },{
+  }, {
     id: "slide3",
     url: "assets/images/my-pic2.jpg",
     title: "My pic 3",
     alt: "My pic 3",
     description: ""
-  },{
+  }, {
     id: "slide4",
     url: "assets/images/my-pic3.jpg",
     title: "My pic 4",
     alt: "My pic 4",
     description: ""
-  },{
+  }, {
     id: "slide5",
     url: "assets/images/my-pic4.jpg",
     title: "My pic 5",
@@ -53,9 +53,9 @@ export class MeCardComponent implements OnDestroy {
 
   constructor(private appState: AppStateService, private router: Router) {
     this.subscription = appState.getHeaderState().subscribe(
-      (isHeaderFix : boolean) => {
+      (isHeaderFix: boolean) => {
         this.isHeaderFix = isHeaderFix;
-        if(isHeaderFix) {
+        if (isHeaderFix) {
           this.play = false;
         } else {
           this.play = true;
@@ -69,16 +69,18 @@ export class MeCardComponent implements OnDestroy {
         if (event instanceof NavigationStart) {
           if (event.url === "/" || event.url === "/home") {
             this.activateScroll = true;
+            setTimeout(() => {
+              this.isInitial = false;
+            }, 2000);
           } else {
             this.activateScroll = false;
+            this.isInitial = false;
           }
           window.scrollTo(0, 0);
         }
       });
 
-    setTimeout(() => {
-      this.isInitial = false;
-    }, 2000);
+
   }
 
   ngOnDestroy() {
