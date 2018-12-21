ef149897a309a6966331bf9a80b6a3a49bfda396
Jayabratha Saha <jaysaha0@publicisgroupe.net>
2018-12-21 18:41:09 +0530

    Adding Google Analytics using gtag.js and upgrading js-ng-carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { AppStateService } from './app-state.service';
 import { ElasticsearchService } from './elasticsearch.service';
-import { Subscription, Observable } from 'rxjs';
-import { Store } from '@ngrx/store';
-import { AppState } from './app.state';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -14,10 +12,34 @@ import { AppState } from './app.state';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private store: Store<AppState>) {
+  constructor(private router: Router, private title: Title) {
   }
 
   ngOnInit() {
-    
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        (<any>window).gtag('config', 'UA-131214369-1', { 'page_path': event.urlAfterRedirects });
+
+        switch (event.urlAfterRedirects) {
+          case '/art':
+            this.title.setTitle('Art | Collection of my artworks');
+            break;
+          case '/projects':
+            this.title.setTitle('Projects | View my hobby projects');
+            break;
+          case '/blog':
+            this.title.setTitle('Blog | My blog articles on technology and life');
+            break;
+          case '/about':
+            this.title.setTitle('About | Learn more about me');
+            break;
+          case '/contact':
+            this.title.setTitle('Contact | Connect with me on social network or reach out to me');
+            break;
+          default:
+            this.title.setTitle('Jayabratha | My Portfolio | See my artworks, projects and blog')
+        }
+      }
+    })
   }
 }
