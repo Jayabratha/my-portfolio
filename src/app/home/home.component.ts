a40b6b73d0dbb3726c06e380d41fbcfb78bfaeed
Jayabratha Saha <jaysaha0@publicisgroupe.net>
2019-01-24 11:44:03 +0530

    Style Optimizations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -56,7 +56,7 @@ export class HomeComponent implements OnInit, OnDestroy {
   stepCount: number = 0;
   decounce: boolean = false;
   isMobile: boolean = false;
-  padding: number = 200;
+  padding: number = 100;
 
   constructor(
     private store: Store<AppState>,
