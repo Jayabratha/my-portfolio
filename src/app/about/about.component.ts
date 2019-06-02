4a57a115b9970d95665e0b04b0edf088ad1373fd
Jay <js.design@outlook.com>
2019-06-02 21:42:05 +0530

    Getting exp from cms and updates

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, ViewChildren, QueryList, ElementRef, Renderer2, OnInit } from '@angular/core';
+import { FlamelinkService } from '../shared/flamelink.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-about',
@@ -8,8 +10,19 @@ import { Component, ViewChildren, QueryList, ElementRef, Renderer2, OnInit } fro
 export class AboutComponent implements OnInit {
   @ViewChildren('skillItem') skillItems: QueryList<ElementRef>;
 
+  experiences: Observable<any[]>;
+
   constructor(
-    private renderer: Renderer2) {
+    private renderer: Renderer2,
+    private flService: FlamelinkService
+  ) {
+    this.flService.getContent("experience").subscribe((exps) => {
+      this.experiences = exps.sort(this.compareByStartDate);
+    });
+  }
+
+  compareByStartDate(a, b) {
+    return parseInt(b.startDate.split(' ')[1], 10) - parseInt(a.startDate.split(' ')[1], 10);
   }
 
   ngOnInit() {
